6903a6eb4d7d34e619b25a330df0fb3438ec265d
gabrielaromero08 <gabrielaromero080517@gmail.com>
2025-03-20 19:34:17 -0500

    base de datos

--- connection.py
+++ connection.py
@@ -3,7 +3,7 @@ from pymongo.server_api import ServerApi
 
 
 def connect_to_mongodb(db_name, collection_name):
-    uri = "mongodb+srv://gabyleal:Diana2024-1@ejercicio1.tttur.mongodb.net/?retryWrites=true&w=majority&appName=EJERCICIO1"
+    uri = "mongodb+srv://gabyleal:<db_password>@ejercicio1.tttur.mongodb.net/?retryWrites=true&w=majority&appName=EJERCICIO1"
     client = MongoClient(uri, server_api=ServerApi('1'))
     db = client[db_name]
     collection = db[collection_name]
