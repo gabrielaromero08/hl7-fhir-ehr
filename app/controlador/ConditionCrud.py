d4fdf95f8a1b6367bc363d14c5fe4abf70e6b674
gabrielaromero08 <gabrielaromero080517@gmail.com>
2025-04-03 16:18:21 -0500

    3

---
+++ app/controlador/ConditionCrud.py
@@ -0,0 +1,26 @@
+from connection import connect_to_mongodb
+from bson import ObjectId
+from fhir.resources.condition import Condition
+import json
+
+# Conectar a la colección de condiciones en MongoDB
+collection = connect_to_mongodb("SampleConditionService", "conditions")
+
+def WriteCondition(condition_dict: dict):
+    try:
+        # Validar la estructura de la condición con el modelo FHIR
+        cond = Condition.model_validate(condition_dict)
+    except Exception as e:
+        return f"errorValidating: {str(e)}", None
+
+    # Convertir el modelo validado a JSON
+    validated_condition_json = cond.model_dump()
+
+    # Insertar la condición en la base de datos
+    result = collection.insert_one(validated_condition_json)
+
+    if result:
+        inserted_id = str(result.inserted_id)
+        return "success", inserted_id
+    else:
+        return "errorInserting", None
